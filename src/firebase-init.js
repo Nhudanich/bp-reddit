6e1968041fc242f9aa20d504e7050f0defa939df
robertvunabandi <rvunabandi@gmail.com>
2019-02-05 22:29:18 -0500

    added docs for the firebase file

--- src/firebase-init.js
+++ src/firebase-init.js
@@ -1,23 +1,27 @@
-// Initialize Firebase
-const config = {
-  apiKey: "AIzaSyCcW3uHlaet4Ql1ZDCuBrw0NNQ9J9Bm0r0",
-  authDomain: "bp-mini-reddit.firebaseapp.com",
-  databaseURL: "https://bp-mini-reddit.firebaseio.com",
-  projectId: "bp-mini-reddit",
-  storageBucket: "bp-mini-reddit.appspot.com",
-  messagingSenderId: "444483323688"
-};
-firebase.initializeApp(config);
-
-// initialize the firebase global variable
+// initialize the firebase global variable and define functions
+// to be used by this Firebase global object. These functions
+// are used inside of main.js.
 const Firebase = {};
 
+/**
+ * loads all posts from the Firebase database
+ * @return {Object<String, Object>} : keys are string ids of
+ *         posts, and values are the actual post contents
+ * @async
+ * */
 Firebase.loadPosts = async function loadPosts() {
   const ref = firebase.database().ref("posts");
   const snapshot = await ref.once("value");
   return snapshot.val();
 };
 
+/**
+ * stores a new post in the database
+ * @param post_object {Object} : an object with keys value pairs:
+ *        (text, String), (posted_on, Number), (username, String)
+ * @void
+ * @async
+ * */
 Firebase.storePost = async function storePost(post_object) {
   // create a new reference child (with a random hash)
   // for the new post to be stored
@@ -28,13 +32,28 @@ Firebase.storePost = async function storePost(post_object) {
   new_child_ref.set(post_object);
 };
 
+/**
+ * updates a post with a new vote value
+ * @param post_id {String} : id of the post
+ * @param username {String} : username that has voted
+ * @param vote {Number} : their vote, either 1, 0, of -1.
+ * @void
+ * @async
+ * */
 Firebase.updateUserVoteToPost = async function updateUserVoteToPost(post_id, username, vote) {
   const ref = firebase.database().ref(`posts/${post_id}/votes/${username}`);
-  // if vote = 0 (i.e. no vote), then this will result
-  // in removing that username from the vote
+  // if vote = 0 (i.e. no vote), then this will result in
+  // removing that username from the vote because
+  // JS automatically casts 0 into a boolean false
   ref.set(vote ? vote : null);
 };
 
+/**
+ * this activates listening to changes in the database so that
+ * we update the application every time a change happens. for
+ * instance, if other users upvote something or post something
+ * @void
+ * */
 Firebase.activateListeningToPosts = function activateListeningToPosts() {
   const ref = firebase.database().ref("posts");
   ref.on("value", App.loadFeed);
